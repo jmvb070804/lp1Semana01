115f3948b4b0e197ab890b37c306f052f8a7918c
João Brito <jmvb070804@gmail.com>
2023-03-03 11:40:40 +0000

    added new project: TiposVarios. Added the variable var1 to the TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    
+    {
+        int var1 = 9;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(var1);
+        }
+    }
+}
