0ec60173f4fff44c9cde639516bea7044af584a1
João Brito <jmvb070804@gmail.com>
2023-03-03 11:25:39 +0000

    corrected the /// in the main from the last commit

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
 namespace OlaMundo
-/// {
+{
     /// class Program
     {
-        static void Main(string[] args)
+        static void /// Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bye, World!");
