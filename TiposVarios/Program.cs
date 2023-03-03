df647e5d6b9b48c351a2560593e6e3f115c14f22
João Brito <jmvb070804@gmail.com>
2023-03-03 11:54:29 +0000

    added to boolean variables: var3 and var4 and made them print in the console

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,9 +6,16 @@ namespace TiposVarios
     
     {
         int var1 = 9;
+        char var2 = "✈teste2©";
+
+        bool var3 = "loren";
+        bool var4 = "ipsum2";
         static void Main(string[] args)
         {
             Console.WriteLine(var1);
+            Console.WriteLine(var2);
+            Console.WriteLine(var3);
+            Console.WriteLine(var4);
         }
     }
 }
